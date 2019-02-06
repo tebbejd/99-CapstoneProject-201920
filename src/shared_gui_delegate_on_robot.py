43d53d7027fe0b710a54eec7cf95e89000bb2631
Jacob Tebbe <tebbejd@rose-hulman.edu>
2019-02-06 14:00:46 -0500

    Updated names on shared gui

--- src/shared_gui_delegate_on_robot.py
+++ src/shared_gui_delegate_on_robot.py
@@ -3,7 +3,7 @@
   This code is the delegate for handling messages from the shared GUI.
 
   Author:  Your professors (for the framework)
-    and PUT_YOUR_NAMES_HERE.
+    and Jacob Tebbe and Brandon Wohlfarth.
   Winter term, 2018-2019.
 """
 
