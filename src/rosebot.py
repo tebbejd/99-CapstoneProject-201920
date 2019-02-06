79c3094c5904d22e99cc773096b7bb5ad1ce48b4
Jacob Tebbe <tebbejd@rose-hulman.edu>
2019-02-06 15:19:08 -0500

    arm and claw class done

--- src/rosebot.py
+++ src/rosebot.py
@@ -220,7 +220,6 @@ class ArmAndClaw(object):
         Move its Arm to the given position, where 0 means all the way DOWN.
         The robot must have previously calibrated its Arm.
         """
-        self.motor.reset_position()
         if self.motor.get_position() > desired_arm_position:
             self.motor.turn_on(-100)
         if self.motor.get_position() < desired_arm_position:
