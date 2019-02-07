c6cdbfeb47575387fbb80763d8cfa600fc7352cd
wohlfab <wohlfab@rose-hulman.edu>
2019-02-07 14:09:36 -0500

    class for the deldgate for the drive system and arm

--- src/m2_run_this_on_robot.py
+++ src/m2_run_this_on_robot.py
@@ -8,7 +8,7 @@
 import rosebot
 import mqtt_remote_method_calls as com
 import time
-
+import shared_gui_delegate_on_robot
 
 def main():
     """
@@ -16,8 +16,8 @@ def main():
       1. Makes the EV3 robot to various things.
       2. Communicates via MQTT with the GUI code that runs on the LAPTOP.
     """
-    run_test_arm()
-
+    #run_test_arm()
+    real_thing()
 
 def run_test_arm():
     robot = rosebot.RoseBot()
@@ -30,6 +30,15 @@ def run_test_arm():
     #time.sleep(0.1)
     robot.arm_and_claw.move_arm_to_position(2500)
     #time.sleep(0.1)
+
+def real_thing():
+    robot = rosebot.RoseBot()
+    delagate_That_Receives = shared_gui_delegate_on_robot.DelagateThatReceives(robot)
+    mqtt_reciever = com.MqttClient(delagate_That_Receives)
+    mqtt_reciever.connect_to_pc()
+
+    while True:
+        time.sleep(0.01)
 
 
 # -----------------------------------------------------------------------------
--- src/shared_gui_delegate_on_robot.py
+++ src/shared_gui_delegate_on_robot.py
@@ -7,3 +7,36 @@
   Winter term, 2018-2019.
 """
 
+class DelegateThatReceives(object):
+
+    def __init__(self, robot):
+        """" :type robot:  rosebot.RoseBot """
+        self.robot = robot
+
+    def forward(self, left_wheel, right_wheel):
+        self.robot.drive_system.go(int(left_wheel), int(right_wheel))
+
+    def backward(self,left_wheel, right_wheel):
+        self.robot.drive_system.go(0-int(left_wheel), 0-int(right_wheel))
+
+    def left(self, left_wheel, right_wheel):
+        self.robot.drive_system.go(0-int(left_wheel), int(right_wheel))
+
+    def right(self, left_wheel, right_wheel):
+        self.robot.drive_system.go(int(left_wheel), 0-int(right_wheel))
+
+    def stop(self):
+        self.robot.drive_system.stop()
+
+    def raise_arm(self):
+        self.robot.ArmAndClaw.raise_arm()
+
+    def lower_arm(self):
+        self.robot.ArmAndClaw.lower_arm()
+
+    def calibrate_arm(self):
+        self.robot.ArmAndClaw.calibrate_arm()
+
+    def arm_to_position(self, pos):
+        self.robot.ArmAndClaw.move_arm_to_position(pos)
+        
