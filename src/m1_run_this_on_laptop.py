a684c6814a5da9602df39c0c9a4d672ad166bf97
Jacob Tebbe <tebbejd@rose-hulman.edu>
2019-02-07 20:16:33 -0500

    updated buttons for control

--- src/m1_run_this_on_laptop.py
+++ src/m1_run_this_on_laptop.py
@@ -31,32 +31,25 @@ def main():
     root = tkinter.Tk()
     root.title('Capstone Project Winter 2019')
 
-
     # -------------------------------------------------------------------------
     # The main frame, upon which the other frames are placed.
     # -------------------------------------------------------------------------
-    frame = ttk.Frame(root,padding = 10,borderwidth = 5,relief = 'groove')
+    frame = ttk.Frame(root, padding=10, borderwidth=5, relief='groove')
     frame.grid()
     # -------------------------------------------------------------------------
     # Sub-frames for the shared GUI that the team developed:
     # -------------------------------------------------------------------------
-    teleop_frame, arm_frame, control_frame = get_shared_frames(frame,sender)
+    teleop_frame, arm_frame, control_frame = get_shared_frames(frame, sender)
 
     # -------------------------------------------------------------------------
     # Frames that are particular to my individual contributions to the project.
     # -------------------------------------------------------------------------
-    # TODO: Implement and call get_my_frames(...)
-    go_straight_for_seconds = shared_gui.get_seconds_frame(frame,sender)
-    go_straight_for_inches_timed = shared_gui.get_inches_time_frame(frame,sender)
-    go_straight_for_inches_encoder = shared_gui.get_inches_encoder_frame(frame,sender)
-    go_straight_for_seconds.grid(row = 0, column = 1)
-    go_straight_for_inches_timed.grid(row = 1, column = 1)
-    go_straight_for_inches_encoder.grid(row = 2,column = 1)
+    # DONE: Implement and call get_my_frames(...)
+    get_my_frames(frame, sender)
     # -------------------------------------------------------------------------
     # Grid the frames.
     # -------------------------------------------------------------------------
-    grid_frames(teleop_frame,arm_frame,control_frame)
-
+    grid_frames(teleop_frame, arm_frame, control_frame)
 
     # -------------------------------------------------------------------------
     # The event loop:
@@ -65,16 +58,30 @@ def main():
 
 
 def get_shared_frames(main_frame, mqtt_sender):
-    teleop_frame = shared_gui.get_teleoperation_frame(main_frame,mqtt_sender)
-    arm_frame = shared_gui.get_arm_frame(main_frame,mqtt_sender)
-    control_frame = shared_gui.get_control_frame(main_frame,mqtt_sender)
+    teleop_frame = shared_gui.get_teleoperation_frame(main_frame, mqtt_sender)
+    arm_frame = shared_gui.get_arm_frame(main_frame, mqtt_sender)
+    control_frame = shared_gui.get_control_frame(main_frame, mqtt_sender)
 
     return teleop_frame, arm_frame, control_frame
 
+
 def grid_frames(teleop_frame, arm_frame, control_frame):
-    teleop_frame.grid(row = 0,column = 0)
-    arm_frame.grid(row = 1,column = 0)
-    control_frame.grid(row = 2 , column = 0)
+    teleop_frame.grid(row=0, column=0)
+    arm_frame.grid(row=1, column=0)
+    control_frame.grid(row=2, column=0)
+
+
+def get_my_frames(frame, sender):
+    go_straight_for_seconds = shared_gui.get_seconds_frame(frame, sender)
+    go_straight_for_inches_timed = shared_gui.get_inches_time_frame(frame, sender)
+    go_straight_for_inches_encoder = shared_gui.get_inches_encoder_frame(frame, sender)
+    grid_my_frames(go_straight_for_seconds, go_straight_for_inches_timed, go_straight_for_inches_encoder)
+
+
+def grid_my_frames(go_straight_for_seconds, go_straight_for_inches_timed, go_straight_for_inches_encoder):
+    go_straight_for_seconds.grid(row=0, column=1)
+    go_straight_for_inches_timed.grid(row=1, column=1)
+    go_straight_for_inches_encoder.grid(row=2, column=1)
 
 
 # -----------------------------------------------------------------------------
