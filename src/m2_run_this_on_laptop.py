329ace9dd260e22d3915551cc6995cd4ef6cb058
wohlfab <wohlfab@rose-hulman.edu>
2019-02-18 16:12:26 -0500

    shared gui delagate section of Brandon's sprint 3

--- src/m2_run_this_on_laptop.py
+++ src/m2_run_this_on_laptop.py
@@ -41,6 +41,7 @@ def sprint3():
     # The main frame, upon which the other frames are placed.
     # -------------------------------------------------------------------------
     main_frame = ttk.Frame(root, padding=10, borderwidth=5, relief="groove")
+
     main_frame.grid()
 
     # -------------------------------------------------------------------------
@@ -53,10 +54,12 @@ def sprint3():
     # DONE: Implement and call get_my_frames(...)
     name_frame = get_name_frame(main_frame, mqtt_sender)
     control_frame = get_control_frame(main_frame, mqtt_sender)
+    teleop_frame, exit_frame = get_shared_frames(main_frame, mqtt_sender)
+
     # -------------------------------------------------------------------------
     # Grid the frames.
     # -------------------------------------------------------------------------
-    grid_frames3(name_frame, control_frame)
+    grid_frames3(name_frame, control_frame, teleop_frame, exit_frame)
     # -------------------------------------------------------------------------
     # The event loop:
     # -------------------------------------------------------------------------
@@ -110,7 +113,7 @@ def get_control_frame(window, sender):
     inspect["command"] = lambda: inspect_func(sender)
 
     #Action 5 Make fun of kids
-    make_fun = ttk.Button(frame, text="Joke with residence")
+    make_fun = ttk.Button(frame, text="Joke with resident")
     make_fun_label = ttk.Label(frame, text="Inspect the freshman's room")
     make_fun_label.grid(row=8, column=2)
     make_fun.grid(row=8, column=0)
@@ -166,10 +169,11 @@ def get_name_frame(window, sender):
     frame_label.grid()
 
     return frame
-def grid_frames3(title_frame, control_frame):
-
+def grid_frames3(title_frame, control_frame, tele_frame, exit_frame):
     title_frame.grid(row = 0, column = 0)
     control_frame.grid(row = 1, column = 0)
+    tele_frame.grid(row = 2, column = 0)
+    exit_frame.grid(row = 3, column = 0)
 
 def frame1_2():
     """
@@ -218,11 +222,10 @@ def frame1_2():
 
 def get_shared_frames(main_frame, mqtt_sender):
     teleop_frame = shared_gui.get_teleoperation_frame(main_frame, mqtt_sender)
-    arm_frame = shared_gui.get_arm_frame(main_frame, mqtt_sender)
     control_frame = shared_gui.get_control_frame(main_frame, mqtt_sender)
 
 
-    return teleop_frame, arm_frame, control_frame
+    return teleop_frame, control_frame
 
 def grid_frames(teleop_frame, arm_frame, control_frame, tone_frame, spin_frame):
     teleop_frame.grid(row = 0, column = 0)
